9c89bb3ca53cbbea3abffd9c28f6238f545c9550
Mauro Picatto <mauropicatto@gmail.com>
2021-07-15 23:07:40 -0300

    cambios en home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 //------------components---------------------
-import NavBar from '../NavBar/index'
+
 import FilterBar from '../FilterBar/index'
 import Graphic from '../Graphic/index'
 import DataTables   from '../Table/index'
-import Footer from '../Footer/index'
+
 
 const Home = () =>{
     return(
